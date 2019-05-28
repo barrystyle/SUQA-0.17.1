784ea8fa3e30f5c5bcdecce491cdbb54faa46d48
barrystyle <barrystyle@westnet.com.au>
2019-05-28 21:36:16 +0800

    disable hd key usage, bump protocol so is clearly visible

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 250000;
+static const int PROTOCOL_VERSION = 250001;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
